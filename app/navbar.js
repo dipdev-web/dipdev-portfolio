c24ec1d6fcdb4f7a841c462de1903ab32ee5244e
Dip Dev <dipdev.web@gmail.com>
2026-06-19 23:23:29 +0100

    Update navbar.js

--- app/navbar.js
+++ app/navbar.js
@@ -59,7 +59,7 @@ export default function Navbar() {
 
       {/* CTA (DESKTOP ONLY) */}
       <a
-        href="https://wa.me/212619831430"
+        href=""
         target="_blank"
         className="hidden md:inline-flex bg-yellow-500 text-black px-4 py-1.5 text-sm rounded-lg font-medium hover:scale-105 transition"
       >
